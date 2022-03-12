b34278d0079b39aed53fc9b3b7992f8ee378371f
Ashish <ashishthehulk@gmail.com>
2022-03-13 01:46:02 +0530

    edit profile, back Button

--- app/src/main/java/io/github/ashishthehulk/ezybites/EditProfileActivity.java
+++ app/src/main/java/io/github/ashishthehulk/ezybites/EditProfileActivity.java
@@ -4,15 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import io.github.ashishthehulk.ezybites.Screens.AddPostActivity;
 
 public class EditProfileActivity extends AppCompatActivity {
+    private ImageView backPost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        backPost = findViewById(R.id.backPost);
+        backPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(EditProfileActivity.this, MainActivity.class);
+                intent.putExtra("frag",2);
+                startActivity(intent);
+                finish();
+
+            }
+        });
     }
     @Override
     public void onBackPressed() {
