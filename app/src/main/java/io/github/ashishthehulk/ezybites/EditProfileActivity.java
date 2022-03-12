5e61ca19b6ca3e7158dae2012a4d1d0be7a9af42
Ashish <ashishthehulk@gmail.com>
2022-03-13 01:43:01 +0530

    edit profile, on back

--- app/src/main/java/io/github/ashishthehulk/ezybites/EditProfileActivity.java
+++ app/src/main/java/io/github/ashishthehulk/ezybites/EditProfileActivity.java
@@ -2,7 +2,10 @@ package io.github.ashishthehulk.ezybites;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import io.github.ashishthehulk.ezybites.Screens.AddPostActivity;
 
 public class EditProfileActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class EditProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        Intent intent = new Intent(EditProfileActivity.this, MainActivity.class);
+        intent.putExtra("frag",3);
+        startActivity(intent);
+        finish();
+
     }
 }
