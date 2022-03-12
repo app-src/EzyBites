1529aaf9608eda66ec9b268d151143da6ffd88d0
hridik <hridikgulati04@gmail.com>
2022-03-13 00:41:19 +0530

    Adding Post Page3

---
+++ app/src/main/java/io/github/ashishthehulk/ezybites/Screens/AddPostScreen.java
@@ -0,0 +1,16 @@
+package io.github.ashishthehulk.ezybites.Screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.github.ashishthehulk.ezybites.R;
+
+public class AddPostScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_post_screen);
+    }
+}
