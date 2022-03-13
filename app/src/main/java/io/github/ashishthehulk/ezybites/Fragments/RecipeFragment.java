5e15703b09af68efa35101da9bc8c64dced9953b
viruj <virujthakur01@gmail.com>
2022-03-13 07:36:38 +0530

    added working add recipe button

--- app/src/main/java/io/github/ashishthehulk/ezybites/Fragments/RecipeFragment.java
+++ app/src/main/java/io/github/ashishthehulk/ezybites/Fragments/RecipeFragment.java
@@ -76,7 +76,7 @@ public class RecipeFragment extends Fragment {
         View view =  inflater.inflate(R.layout.fragment_recipe, container, false);
 
         String[] temp={"Baingan ka Bharta","Chicken Handi","Dal fry","Kidney Bean Curry","Lamb Biryani"};
-        String[] ingre={"1. Brinjal\n2. Ghee\n3. Red Chillis\n4. Garlic","1. Brinjal\n 2. Ghee\n3. Red Chillis\n4. Garlic","1. Brinjal\n 2. Ghee\n3. Red Chillis\n4. Garlic","1. Brinjal\n 2. Ghee\n3. Red Chillis\n4. Garlic","1. Brinjal\n 2. Ghee\n3. Red Chillis\n4. Garlic"};
+        String[] ingre={"1. Brinjal 100 gm\n2. Ghee 2tbsp\n3. Red Chillis 3-4pcs\n4. Garlic 2 cloves","1. Chicken 1pc\n 2. Ghee 2tbsp\n3. ChickenMasala 2tsp\n4. Garlic 1 clove","1. Toor Dal 200gm\n 2. Ghee 2tbsp\n3. Tomatos 3-4pcs\n4. Coriander 20gm","1. Kidney Beans 200gm\n 2. Onions 100gm\n3. Garam Masala 2tsp\n4. Garlic 2cloves","1. Lamb 500gm\n 2. Oil 2tbsp\n3. Turmeric 10gm\n4. Tomato 3-4pcs"};
         int[] temp2={R.drawable.foodrecipeimage1,R.drawable.foodrecipeimage2,R.drawable.foodrecipeimage3,R.drawable.foodrecipeimage4,R.drawable.foodrecipeimage5};
         int[] temp3={R.drawable.veg,R.drawable.nonveg,R.drawable.veg,R.drawable.veg,R.drawable.nonveg};
 
